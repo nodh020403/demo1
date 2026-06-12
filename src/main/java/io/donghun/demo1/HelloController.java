05f0ddc1dab54958c220406ed164764531875c4c
nodh020403 <nodh020403n@naver.com>
2026-06-12 09:47:50 +0900

    first commit

--- src/main/java/io/donghun/demo1/HelloController.java
+++ src/main/java/io/donghun/demo1/HelloController.java
@@ -1,4 +1,14 @@
 package io.donghun.demo1;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class HelloController {
+
+    @GetMapping("/hello")
+    public String sayHello(){
+        return "Hello, World!";
+    }
 }
